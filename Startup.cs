ceb16090c2ff29509f5eda8dc5547aab8c7be2fd
Reno594 <jdieppacastillo@gmail.com>
2024-12-25 14:27:53 +0100

    Configurar archivos estáticos y añadir contenido a index.html

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+    app.UseHttpsRedirection();
+    app.UseStaticFiles(); // Asegúrate de que esta línea esté presente
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+}
